0efa48e0a0ce3f2e82c6e56666354e9045037575
juand0014 <juand0014@hotmail.com>
2022-11-09 13:11:26 -0400

    fixture(resource): add resource image (post and get)

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,15 +1,35 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors, BadRequestException } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors, BadRequestException, Res } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { fileFilter } from 'src/helpers/fileFilter.helper';
+import { Response } from 'express';
+import { diskStorage } from 'multer';
+import { fileFilter, fileNamer } from 'src/helpers';
 import { FilesService } from './files.service';
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configServices: ConfigService
+    ) {}
+
+  @Get('product/:imageName')
+  findProductImage(
+    @Res() res: Response,
+    @Param('imageName') imageName: string
+  ){
+    const path = this.filesService.getStaticProducImage(imageName)
+    res.sendFile(path);
+  }
 
   @Post('product')
   @UseInterceptors( FileInterceptor('file', {
-    fileFilter: fileFilter
+    fileFilter: fileFilter,
+    // limits: { fileSize: 1024 * 1024 * 5 }
+    storage: diskStorage({
+      destination: './static/products',
+      filename: fileNamer
+    })
   }) )
   uploadFileImage( 
     @UploadedFile() file: Express.Multer.File
@@ -19,9 +39,10 @@ export class FilesController {
       throw new BadRequestException('No file provided');
     }
 
+    const secureUrl = `${this.configServices.get('HOST_API')}/api/files/${file.filename}`
+
     return {
-      fileName: file.originalname,
+      secureUrl 
     }
   }
- 
 }
